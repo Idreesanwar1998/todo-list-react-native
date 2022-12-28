56b507102f725b0e899144f8081bc7a7d50e3d82
Idrees Anwar <idrees.anwar@abrdn.com>
2022-12-28 14:43:51 +0000

    navigation between screens on click now works

--- app/pages/TodoItemsScreen.js
+++ app/pages/TodoItemsScreen.js
@@ -1,6 +1,9 @@
 import { View, Text } from 'react-native';
 
 export default function TodoItemsScreen() {
+    let t = fetch('https://jsonplaceholder.typicode.com/todos/2')
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     return (
         <View>
             <Text>This is the todo Items Screen</Text>
